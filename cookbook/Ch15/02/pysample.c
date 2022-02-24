70611c332dae0d05dc11ec6246557bbee908bb4a
Iann <iann1978@163.com>
2022-02-24 14:09:53 +0800

    + (cookbook/15/02) Add demo distutils.

---
+++ cookbook/Ch15/02/pysample.c
@@ -0,0 +1,49 @@
+/* pysample.c */
+
+#include "Python.h"
+#include "sample.h"
+
+/* int gcd(int, int) */
+static PyObject *py_gcd(PyObject *self, PyObject *args) {
+	int x, y, result;
+	if (!PyArg_ParseTuple(args, "ii", &x, &y)) {
+		return NULL;
+	}
+	result = gcd(x,y);
+	return Py_BuildValue("i", result);
+}
+
+/* int in_mandel(double, double, int) */
+
+static PyObject *py_in_mandel(PyObject *self, PyObject *args) {
+
+	double x0, y0;
+	int n;
+	int result;
+	
+	if (!PyArg_ParseTuple(args, "ddi", &x0, &y0, &n)) {
+		return NULL;
+	}
+	result = in_mandel(x0,y0,n);
+	return Py_BuildValue("i", result);
+}
+
+static PyMethodDef SampleMethods[] = {
+	{"gcd", py_gcd, METH_VARARGS, "Greatest common divisor"},
+	{"in_mandel", py_in_mandel, METH_VARARGS, "Mandelbrot test"},
+	{NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef samplemodule = {
+	PyModuleDef_HEAD_INIT,
+	"sample",
+	"A sample module",
+	-1,
+	SampleMethods
+};
+
+/* Module initialization function */
+PyMODINIT_FUNC
+PyInit_sample(void) {
+	return PyModule_Create(&samplemodule);
+}
---
+++ cookbook/Ch15/02/sample.h
@@ -0,0 +1,6 @@
+/* sample.h */
+
+#include <math.h>
+
+extern int gcd(int,int);
+extern int in_mandel(double x0, double y0, int n);
